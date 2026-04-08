8ea8d3a4818e668e7a3c549ac69bdd34c3aa947d
Umer Saleem <162819866+UmerSaleemUltra@users.noreply.github.com>
2026-04-08 09:35:28 -0700

    Remove Shopify from US access list
    Removed Shopify entry from the US access section.

--- components/sections/us-access.tsx
+++ components/sections/us-access.tsx
@@ -13,10 +13,7 @@ const platforms = [
     name: "Amazon",
     logo: "https://cdn.brandfetch.io/idawOgYOsG/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B",
   },
-  {
-    name: "Shopify",
-    logo: "https://cdn.brandfetch.io/idC0T6HUjp/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B",
-  },
+  
   {
     name: "eBay",
     logo: "https://cdn.brandfetch.io/idC0T6HUjp/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B",
